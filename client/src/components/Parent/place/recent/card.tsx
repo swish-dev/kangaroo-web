8de0b00ba47ea46706a8e9e8e792b113b0b195db
greatSumini <greatsumini@gmail.com>
2021-05-23 05:55:46 +0900

    feat: :sparkles: complete RecentReservationCard

--- client/src/components/Parent/place/recent/card.tsx
+++ client/src/components/Parent/place/recent/card.tsx
@@ -1,7 +1,9 @@
+import dayjs from 'dayjs'
 import styled from 'styled-components'
+
 import { Color } from '../../../Shared/contant/color.enum'
 import { IJourneyInfo } from '../../../Shared/interfaces/Journey.interface'
-import { getDate } from '../../../Shared/utils/parser'
+import StarRate from '../../components/StarRate'
 
 export type RecentReservationCardProps = IJourneyInfo
 
@@ -11,22 +13,25 @@ export default function RecentReservationCard({
   arriveRouteEdge,
 }: RecentReservationCardProps) {
   const { name, avatarUrl } = driver
-  const arriveDate = getDate(arriveAt)
 
   return (
     <Wrapper>
       <PlaceAndDate>
-        <P style={{ color: 'white' }}>{arriveRouteEdge.name}</P>
-        <P fw="normal" fs="1.4rem">
-          {arriveDate}
+        <P style={{ color: 'white', opacity: 0.7 }}>{arriveRouteEdge.name}</P>
+        <P style={{ color: 'white', opacity: 0.3 }} fw="normal" fs="1.4rem">
+          {dayjs(arriveAt).format('MM/DD')}
         </P>
       </PlaceAndDate>
       <DriverInfo>
-        <ProfileInfo>
-          <Img src={avatarUrl} />
-          <P fs="1.4rem">{name}</P>
-        </ProfileInfo>
-        <StarRate />
+        <Img src={avatarUrl} />
+        <P style={{ color: 'white', opacity: 0.3 }} fs="1.4rem">
+          {name}
+        </P>
+        <StarRate
+          wrapperStyle={{ marginLeft: 'auto' }}
+          score={4}
+          offset="0.1rem"
+        />
       </DriverInfo>
     </Wrapper>
   )
@@ -44,20 +49,19 @@ const Wrapper = styled.div`
 
 const PlaceAndDate = styled.div`
   width: 100%;
+  margin-bottom: 0.8rem;
+
   display: flex;
   justify-content: space-between;
-`
-const DriverInfo = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-`
-const ProfileInfo = styled.div`
-  width: 70%;
-  display: flex;
 `
 
-const StarRate = styled.div``
+const DriverInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  width: 100%;
+`
 
 const P = styled.p<{ fw?: string; fs?: string }>`
   font-weight: ${(props) => (props.fw ? props.fw : 'bold')};
