51da1faeb151b854afa6613cd38a35e24ecae4ed
Yang <ybh1760@gmail.com>
2021-05-22 16:12:35 +0900

    feat(client): :art: add react-cookie and enable sub-route(add exact prop to Route)

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,10 +1,10 @@
-import { Switch, Route } from "react-router-dom"
-import routes from "./components/routes"
+import { Switch, Route } from 'react-router-dom'
+import routes from './components/routes'
 
 function Router() {
   const generateRoutes = () =>
     routes.map((route, index) => (
-      <Route key={index} path={route.path}>
+      <Route exact key={index} path={route.path}>
         <route.component />
       </Route>
     ))
