c6e454d2097b56e5d3f88356bb9e4804763f06c0
greatSumini <greatsumini@gmail.com>
2021-05-23 06:53:59 +0900

    refactor: :recycle: refactor MonitorPage component

--- client/src/components/Monitor/index.tsx
+++ client/src/components/Monitor/index.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import classNames from 'classnames'
+
 import { MarkerImageType } from '../Shared/interfaces/Marker.interface'
 import { IDriverInfo } from '../Shared/interfaces/Driver.interface'
 import { IMarker } from '../Shared/interfaces/Marker.interface'
@@ -7,73 +9,101 @@ import { usePollingFetch } from '../Shared/hooks'
 
 import Map from '../Shared/Map'
 import DriverDetail from './DriverDetail'
-import DriverList from './DriverList'
-import UserList from './UserList'
 import styles from './style.scss'
 
-import classNames from 'classnames'
+import { getUsers } from '../Shared/services/user'
+import { IUserInfo } from '../Shared/interfaces/User.interface'
 const cx = classNames.bind(styles)
 
-const SIDEBAR_MENU = {
-  DRIVER: 'DRIVER',
-  USER: 'USER',
+export enum MonitorSidebarMenuType {
+  Driver,
+  User,
 }
 
-function Monitor() {
-  const [markers, setMarkers] = useState<IMarker[]>([])
-  const [selectedDriver, setSelectedDriver] = useState<IDriverInfo | null>(null)
-  const { data: drivers } = usePollingFetch(getDrivers, 1000) || []
+const { Driver, User } = MonitorSidebarMenuType
+const { CAR_FREE, CAR_OCCUPIED } = MarkerImageType
 
-  useEffect(() => {
-    const driverMarkers =
-      drivers?.map((driver) => {
-        const { lat, lng, journeys } = driver
-        return {
-          lat,
-          lng,
-          imageType:
-            journeys.length === 0 ||
-            journeys[journeys.length - 1]?.status !== 'DRIVING'
-              ? MarkerImageType.CAR_FREE
-              : MarkerImageType.CAR_OCCUPIED,
-          onClick: () => setSelectedDriver(driver),
-        }
-      }) || []
-    setMarkers(driverMarkers)
-  }, [drivers])
+const isDriver = (input: unknown): input is IDriverInfo => {
+  return input != null && (input as IDriverInfo).fmsReport !== undefined
+}
 
-  const [sidebarMenu, setSidebarMenu] = useState(SIDEBAR_MENU.USER)
+export default function MonitorPage() {
+  const [menuType, setMenuType] = useState(User)
+
+  const { data: users } = usePollingFetch(getUsers, 1000)
+
+  const { data: drivers } = usePollingFetch(getDrivers, 700)
+  const [selectedDriverId, setSelectedDriverId] = useState<string | null>(null)
+
+  const selectedDriver = drivers?.find(({ id }) => id === selectedDriverId)
+
+  const handleDriverSelect = (id: string | null) => () => {
+    setSelectedDriverId(id)
+  }
+
+  const clearSelectedDriver = handleDriverSelect(null)
+
+  const markers: IMarker[] =
+    drivers?.map(({ id, lat, lng, journeys }) => ({
+      lat: lat,
+      lng: lng,
+      imageType:
+        journeys?.reverse()[0]?.status === 'DRIVING' ? CAR_OCCUPIED : CAR_FREE,
+      onClick: handleDriverSelect(id),
+    })) || []
+
+  const handleSidebarChange = (input: MonitorSidebarMenuType) => () => {
+    setMenuType(input)
+  }
 
   return (
     <div className="monitor">
       <div className="sidebar">
-        {selectedDriver ? (
-          <DriverDetail {...{ setSelectedDriver, driver: selectedDriver }} />
-        ) : (
+        {selectedDriver && (
+          <DriverDetail onBack={clearSelectedDriver} {...selectedDriver} />
+        )}
+        {!selectedDriver && (
           <>
             <div className="select">
               <p
                 className={cx('selected-base', {
-                  selected: sidebarMenu === SIDEBAR_MENU.USER,
+                  selected: menuType === User,
                 })}
-                onClick={() => setSidebarMenu(SIDEBAR_MENU.USER)}
+                onClick={handleSidebarChange(User)}
               >
                 User
               </p>
               <p
                 className={cx('selected-base', {
-                  selected: sidebarMenu === SIDEBAR_MENU.DRIVER,
+                  selected: menuType === Driver,
                 })}
-                onClick={() => setSidebarMenu(SIDEBAR_MENU.DRIVER)}
+                onClick={handleSidebarChange(Driver)}
               >
                 Driver
               </p>
             </div>
-            {sidebarMenu === SIDEBAR_MENU.USER ? (
-              <UserList />
-            ) : (
-              <DriverList {...{ setSelectedDriver }} />
-            )}
+            <ul className="menu-list">
+              {(menuType === User ? users : drivers)?.map(
+                (record: IUserInfo | IDriverInfo) => {
+                  const { id, journeys } = record
+
+                  return (
+                    <li
+                      className="menu-row"
+                      key={id}
+                      onClick={handleDriverSelect(id)}
+                    >
+                      <p>
+                        <b>
+                          {isDriver(record) ? record.name : record.nickname}
+                        </b>
+                      </p>
+                      <p>{journeys?.reverse()[0]?.status?.toLowerCase()}</p>
+                    </li>
+                  )
+                }
+              )}
+            </ul>
           </>
         )}
       </div>
@@ -81,5 +111,3 @@ function Monitor() {
     </div>
   )
 }
-
-export default Monitor
