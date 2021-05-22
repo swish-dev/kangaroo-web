f9913df98b791976f483f0b205c5bdb1cf90da68
Yang <ybh1760@gmail.com>
2021-05-22 14:11:13 +0900

    feat(client): :bug: resolve build error

--- client/src/components/Monitor/index.tsx
+++ client/src/components/Monitor/index.tsx
@@ -1,26 +1,7 @@
 import './style.scss'
-import SubSample from './SubSample/index'
 
 function Monitor() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="Button">Hello</button>
-        <SubSample name="Junsoo" />
-      </header>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default Monitor
---
+++ client/src/components/Parent/layouts/main.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export function MainLayout() {
+  return <Wrapper></Wrapper>
+}
+
+const Wrapper = styled.div``
