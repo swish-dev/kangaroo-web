3930eb9010afec942a83957b20c93f28b2a7a923
junsoo <jyhckb@naver.com>
2021-05-22 15:22:23 +0900

    feat(client): Add marker dynamically

--- client/src/components/Monitor/index.tsx
+++ client/src/components/Monitor/index.tsx
@@ -1,10 +1,24 @@
+import { MarkerImageType } from '../Shared/interfaces/Marker.interface'
+
 import Map from '../Shared/Map'
 import './style.scss'
 
 function Monitor() {
+  const markers = [
+    {
+      imageType: MarkerImageType.CAR_FREE,
+      lat: 37.56572474944695,
+      lng: 126.97737353796997,
+    },
+    {
+      imageType: MarkerImageType.CAR_OCCUPIED,
+      lat: 37.567106516757505,
+      lng: 126.97727818430188,
+    },
+  ]
   return (
     <div className="App">
-      <Map />
+      <Map {...{ markers }} />
     </div>
   )
 }
