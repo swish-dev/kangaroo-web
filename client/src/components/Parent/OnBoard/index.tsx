c3a51e2dd2e6628a8b45f73ef283e71b42d49b92
junsoo <jyhckb@naver.com>
2021-05-23 01:48:53 +0900

    feat(client): drivers applied

--- client/src/components/Parent/OnBoard/index.tsx
+++ client/src/components/Parent/OnBoard/index.tsx
@@ -1,10 +1,14 @@
 import MainLayout from '../layouts/main'
 import KidsPart from '../main/kids'
+import './style.scss'
 
 export default function OnBoard() {
   return (
     <MainLayout>
       <KidsPart />
+      <div>
+        <p>{`${true} is moving!`}</p>
+      </div>
     </MainLayout>
   )
 }
