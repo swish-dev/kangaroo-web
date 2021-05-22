cc00ee76ac9cbfb9838591a6c4c84c177c9c24aa
greatSumini <greatsumini@gmail.com>
2021-05-23 06:02:35 +0900

    feat(api): :sparkles: add useDriver hook

--- client/src/components/Parent/hooks/driver.hook.ts
+++ client/src/components/Parent/hooks/driver.hook.ts
@@ -1,6 +1,25 @@
 import { useEffect, useState } from 'react'
 import { IDriverInfo } from '../../Shared/interfaces/Driver.interface'
-import { getDrivers } from '../../Shared/services/driver'
+import { getDriverById, getDrivers } from '../../Shared/services/driver'
+
+export const useDriver = (id: string) => {
+  const [driver, setDriver] = useState<IDriverInfo | null>(null)
+
+  const fetch = async () => {
+    const data = await getDriverById(id)
+    setDriver(data)
+  }
+
+  useEffect(() => {
+    if (!id) {
+      return
+    }
+    fetch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
+  return driver
+}
 
 /** Get All Drivers */
 export const useDrivers = () => {
